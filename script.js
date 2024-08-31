f92bab896cf3c1b4452614559222a9e0a1eb66ba
MichaelPro777 <163635124+MichaelPro777@users.noreply.github.com>
2024-08-31 16:18:43 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let page = 1;
 
 async function searchImages(){   
     keyword = searchBox.value;
-    const url =`https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+    const url =`https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
 
 const response = await fetch(url); 
 const data = await response.json(); 
@@ -37,8 +37,8 @@ results.map((result) =>{
 showMoreBtn.style.display = "block";
 }
 
-searchForm.addEventListener("submit", (e) =>{ //??
-    e.preventDefault(); //??
+searchForm.addEventListener("submit", (e) =>{ 
+    e.preventDefault(); 
     page = 1;
     searchImages();
 });
